15d644702e67bd755a72238bca0359246a195bc5
alex4851 <alexisbeligne@gmail.com>
2024-10-21 17:13:00 +0200

    try 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './i18n';
-import { useTranslation } from 'react-i18next';
-
 import './style/App.css';
 import Nav from './Nav.js';
 import Home from './Home.js';
 import About from './About.js';
 import Experience from './Experience.js';
 import Footer from './Footer.js';
-import Skill from './Skill.js';
 import { ThemeProvider } from './ThemeContext.js';
 
 
